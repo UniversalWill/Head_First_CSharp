bb1248e28ce402d4c1b2b076c257fd976f421b97
Gena Ivaschenko <gena9902@gmail.com>
2022-10-14 15:06:06 +0600

    going book

---
+++ Head_First_CSharp/AbilityScoreTester/AbilityScoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace AbilityScoreTester
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+        public void CalculateAbilityScore()
+        {
+            // Результат броска делится на значение поля DivideBy
+            double divided = RollResult / DivideBy;
+            // AddAmount прибавляется к результату деления
+            int added = AddAmount + (int)divided;
+            // Если результат слишком мал, использовать значение Minimum
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+}
